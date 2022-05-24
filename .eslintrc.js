3a4f50782087ddc748965c7b940e09a7ff0b5f4b
Pau Baguer <pauinki@gmail.com>
2022-05-24 13:10:17 +0200

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,17 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    'standard'
-  ],
-  parser: '@typescript-eslint/parser',
+  extends: ["standard", "plugin:prettier/recommended"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    '@typescript-eslint'
-  ],
+  plugins: ["@typescript-eslint"],
   rules: {
-  }
-}
+    "prettier/prettier": "warn",
+    //   ignore: ["node_modules", "package.json", "package-lock.json"],
+  },
+};
